13a6a5a693d6a7fb597268252af597c891f4af17
ayush <ayush.tamta8@gmail.com>
2026-05-13 21:27:33 +0530

    gem

--- main.py
+++ main.py
@@ -1,9 +1,18 @@
 from fastapi import FastAPI
+import google.generativeai as genai
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+genai.configure(
+    api_key=os.getenv(AIzaSyBkpXQ76SxtK6rfIAQDvmchJw7F39L_jSU)
+)
 
 app = FastAPI()
 
 @app.get("/")
 def home():
     return {
-        "message": "Backend working"
+        "message": "Gemini backend working"
     }
