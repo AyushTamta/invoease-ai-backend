6636fbc8ea2993af6aac0a9c3533ca8bef61362e
ayush <ayush.tamta8@gmail.com>
2026-05-13 21:15:54 +0530

    test

--- services/supabase_service.py
+++ services/supabase_service.py
@@ -6,12 +6,28 @@ from supabase import create_client
 
 load_dotenv()
 
-supabase = create_client(
-    os.getenv("SUPABASE_URL"),
-    os.getenv("SUPABASE_SERVICE_ROLE")
+SUPABASE_URL = os.getenv("SUPABASE_URL")
+SUPABASE_SERVICE_ROLE = os.getenv(
+    "SUPABASE_SERVICE_ROLE"
 )
 
+supabase = None
+
+if (
+    SUPABASE_URL
+    and SUPABASE_SERVICE_ROLE
+):
+    supabase = create_client(
+        SUPABASE_URL,
+        SUPABASE_SERVICE_ROLE
+    )
+
 def save_invoice(data):
+
+    if not supabase:
+        return {
+            "error": "Supabase not configured"
+        }
 
     response = supabase.table(
         "invoices"
