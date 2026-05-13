c0a49fe2165f8ff8146dd572768f7b84e50db70a
ayush <ayush.tamta8@gmail.com>
2026-05-13 21:38:38 +0530

    okay

--- main.py
+++ main.py
@@ -1,18 +1,45 @@
 from fastapi import FastAPI
-import google.generativeai as genai
+
 import os
+import requests
+
 from dotenv import load_dotenv
 
 load_dotenv()
 
-genai.configure(
-    api_key=os.getenv(AIzaSyBkpXQ76SxtK6rfIAQDvmchJw7F39L_jSU)
-)
-
 app = FastAPI()
+
+GEMINI_API_KEY = os.getenv(
+    "GEMINI_API_KEY"
+)
 
 @app.get("/")
 def home():
+
     return {
-        "message": "Gemini backend working"
+        "message": "InvoEase AI Backend Running"
     }
+
+@app.get("/test-gemini")
+def test_gemini():
+
+    url = f"https://generativelanguage.googleapis.com/v1beta/models/gemini-2.0-flash:generateContent?key={GEMINI_API_KEY}"
+
+    payload = {
+        "contents": [
+            {
+                "parts": [
+                    {
+                        "text": "Say hello from InvoEase AI backend"
+                    }
+                ]
+            }
+        ]
+    }
+
+    response = requests.post(
+        url,
+        json=payload
+    )
+
+    return response.json()
