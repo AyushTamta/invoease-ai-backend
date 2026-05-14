ad03933c92cbed042865d7ded29cc5bc051eae05
ayush <ayush.tamta8@gmail.com>
2026-05-14 14:33:48 +0530

    scanner

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
-from fastapi import FastAPI
+from fastapi import (
+    FastAPI,
+    UploadFile,
+    File
+)
 
 import os
 import requests
+import base64
 
 from dotenv import load_dotenv
 
@@ -17,11 +22,33 @@ GEMINI_API_KEY = os.getenv(
 def home():
 
     return {
-        "message": "InvoEase AI Backend Running"
+        "message":
+        "InvoEase AI Backend Running"
     }
 
-@app.get("/test-gemini")
-def test_gemini():
+@app.post("/scan-invoice")
+async def scan_invoice(
+    file: UploadFile = File(...)
+):
+
+    image_bytes = await file.read()
+
+    base64_image = base64.b64encode(
+        image_bytes
+    ).decode("utf-8")
+
+    prompt = """
+    Analyze this invoice or receipt.
+
+    Extract:
+    - store_name
+    - total_amount
+    - invoice_date
+    - category
+    - items
+
+    Return ONLY valid JSON.
+    """
 
     url = f"https://generativelanguage.googleapis.com/v1beta/models/gemini-2.0-flash:generateContent?key={GEMINI_API_KEY}"
 
@@ -30,7 +57,16 @@ def test_gemini():
             {
                 "parts": [
                     {
-                        "text": "Say hello from InvoEase AI backend"
+                        "text": prompt
+                    },
+                    {
+                        "inline_data": {
+                            "mime_type":
+                            "image/jpeg",
+
+                            "data":
+                            base64_image
+                        }
                     }
                 ]
             }
